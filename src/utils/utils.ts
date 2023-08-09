49d652bc4c24eff8fd35bb6700c0a142fe1c442e
albedim <dimaio.albe@gmail.com>
2023-08-09 11:54:19 +0200

    [] - env change

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,10 +1,10 @@
 //export const BASE_URL = "http://localhost:5000/api/v1"
-export const BASE_FE_URL = "http://localhost:3000"
+//export const BASE_FE_URL = "http://localhost:3000"
 
 export const BASE_URL = "https://contrycode.pythonanywhere.com/api/v1"
-//export const BASE_FE_URL = "https://contrycode.pages.dev"
+export const BASE_FE_URL = "https://contrycode.pages.dev"
 
 export const colors = {
-  light: ["#ffffff", "#000000", "#fafafa", "#e5e7eb", "#f1f1f1", "gray", "darkgray"],
-  dark: ["#010409", "#e0e0e0", "#0d1117", "#21261d", "#040508", "darkgray", "gray"]
+  light: ["#ffffff", "#000000", "#fafafa", "#e5e7eb", "#f1f1f1"],
+  dark: ["#010409", "#e0e0e0", "#0d1117", "#21261d", "#040508"]
 }
