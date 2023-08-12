77552deb79bedf06aa555f4da936c0ef631c347f
albedim <dimaio.albe@gmail.com>
2023-08-12 23:57:19 +0200

    [bug fixing] - notifications

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,5 @@
 //export const BASE_URL = "http://localhost:5000/api/v1"
-export const BASE_FE_URL = "http://localhost:3000"
+//export const BASE_FE_URL = "http://localhost:3000"
 
 export const BASE_URL = "https://contrycode.pythonanywhere.com/api/v1"
-//export const BASE_FE_URL = "https://contrycode.pages.dev"
+export const BASE_FE_URL = "https://contrycode.pages.dev"
