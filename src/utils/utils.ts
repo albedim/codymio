254e168f5e315feb2cec70e14720173c9a191a95
albedim <dimaio.albe@gmail.com>
2023-08-27 03:10:37 +0200

    [feature] - rebrand

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,4 +2,4 @@
 //export const BASE_FE_URL = "http://localhost:3000"
 
 export const BASE_URL = "https://contrycode.pythonanywhere.com/api/v1"
-export const BASE_FE_URL = "https://contrycode.pages.dev"
+export const BASE_FE_URL = "https://codymio.pages.dev"
